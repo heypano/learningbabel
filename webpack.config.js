acdd215213eb1db23675dc51dba8646d9c6ddd78
Pano Papadatos <pano.papadatos@publicrelay.com>
2017-12-29 10:13:27 -0500

    integrating babel with webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const entryFile = "./src/main.js";
+const distPath = path.resolve(__dirname, "dist");
+const bundleName = 'bundle.js';
+
+module.exports = {
+    entry: entryFile,
+    output: {
+        path: distPath,
+        filename: bundleName
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/, // Which files to bundle
+                exclude: /node_modules/, // Which files NOT to bundle
+                loader: 'babel-loader' // What transform to apply
+            }
+        ]
+    }
+}
